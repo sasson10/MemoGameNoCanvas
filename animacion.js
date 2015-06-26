11ef02cf6e3e7e24184c54c5353e7385ca07aa34
unknown <JITOLEDO@CIR2160MJ01US6S.tveducativa.mx>
2015-06-26 17:57:52 -0500

    sirve, falta acomodo aleatorio

--- animacion.js
+++ animacion.js
@@ -4,10 +4,26 @@ $(document).ready(function() {
     $('.flip').toggle(
             function() {
             	$( this ).find ('.card').addClass('flipped');
+            	id = $( this ).attr('id');
+            	console.log (id);
             },
             function() {
                 $( this ).find ('.card').removeClass('flipped');
             }
     );
+
+    /*function clickFunction(){
+    	alert("diste click en la imagen");
+    }*/
+
+    this.clickFunction = function(e){
+    	alert("diste click en la imagen");
+    	var Imagen1 = document.getElementsByClassName("contenido");
+    	console.log(Imagen1);
+    	//Imagen1.getAttribute("src");
+    	console.log(e.getAttribute("src"));
+    	//var src1 = Imagen1.getAttribute("src");
+    	//console.log(src1);
+    };
 
 });
